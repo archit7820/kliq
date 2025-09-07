f5e2dde9b214ec1dddc65eb46e601c2e88a3d804
Archit <archit.mymail@gmail.com>
2025-09-07 14:59:14 +0530

    Add Capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kelp.app',
+  appName: 'Kelp',
+  webDir: 'build', // or "www" depending on your setup
+  bundledWebRuntime: false
+};
+
+export default config;
